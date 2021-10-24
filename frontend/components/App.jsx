9bbcdf62d8c7b35f140d3616935cd14e9a215907
ggmu1986 <gt2186@gmail.com>
2021-10-24 10:02:43 -0400

    Refactored Ninjas component to Functional and added ternary op based on age

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Ninjas from "./Ninjas";
 
 class App extends React.Component {
+
+    state = {
+        ninjas: [
+            { name: 'Ryu', age: 30, belt: 'Black', id: 1 },
+            { name: 'Yoshi', age: 20, belt: 'Green', id: 2 },
+            { name: 'Ariel', age: 25, belt: 'Orange', id: 3 }
+        ]
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Welcome!</h1>
-                <Ninjas name="Ryu" age="25" belt="black"/>
+                <Ninjas ninjas={this.state.ninjas}/> 
             </div>
         )
     }
--- frontend/components/Ninjas.jsx
+++ frontend/components/Ninjas.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 
-class Ninjas extends Component {
-    render () {
-        const { name, age, belt } = this.props 
-        return (
-            <div className="ninja">
-              <div>Name: {name}</div>  
-              <div>Age: {age}</div>  
-              <div>Belt: {belt}</div>  
-            </div>
-        )
-    }
+const Ninjas = ({ ninjas }) => {
+    // const ninjaList = ninjas.map(ninja => { 
+    //     return (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>Name: { ninja.name }</div>  
+    //             <div>Age: { ninja.age }</div>  
+    //             <div>Belt: { ninja.belt }</div>  
+    //         </div>
+    //     )
+    //     })
+    const ninjaList = ninjas.map(ninja => {
+       return ninja.age > 20 ? (
+        <div className="ninja" key={ninja.id}>
+            <div>Name: { ninja.name }</div>
+            <div>Age: { ninja.age }</div>  
+            <div>Belt: { ninja.belt }</div>  
+        </div>
+       ) : null; 
+    });
+    return (
+        <div className="nina-list">
+            { ninjaList }
+        </div>
+    )
+
 } 
 
 export default Ninjas;
