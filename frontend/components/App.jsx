2ad54ddc05a6b2cd2400f72cf2624ed6b6bcf3a8
ggmu1986 <gt2186@gmail.com>
2021-10-24 09:14:40 -0400

    Added App root component and rendered to DOM thru entry file

---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Welcome!</h1>
+                <p>React is good!</p>
+            </div>
+        )
+    }
+};
+
+export default App;
